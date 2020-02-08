5a2d3fad5b3c0d0dc8c151678a08205f09b876be
elissaurio <elissaurio@hotmail.com>
2020-02-08 10:28:11 -0600

    iniciando

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import {
+  BrowserRouter as Router
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+   <Router>
+
+   </Router>
       
-    </div>
+   
   );
 }
 
---
+++ src/layouts/BaseLayout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BaseLayout = () => {
+
+    return (
+        <h1>Hello</h1>
+    );
+};
+
+
+export default BaseLayout;
