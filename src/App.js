191d39cc9a45dbcdec0147c43ad35abd5e1498d3
elissaurio <elissaurio@hotmail.com>
2020-02-08 10:28:49 -0600

    pages and layouts

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import {
   BrowserRouter as Router
 } from 'react-router-dom';
 
+//Layouts
+import BaseLayout from './layouts/BaseLayout';
+//Pages
+import HomePages from './pages/HomePages';
+
+
 function App() {
   return (
    <Router>
-
+<BaseLayout>
+<Switch>
+  <Route path="/" component={HomePages} />
+ 
+</Switch>
+</BaseLayout>
    </Router>
       
    
   );
-}
+};
+
+
+//Route properties
+//path
+//component
+//exact
+//render: element React
+//children: react elements inside route
+
 
 export default App;
---
+++ src/pages/HomePages.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BaseLayout from '../layouts/BaseLayout';
+
+const HomePage = () => {
+    return (
+        <>
+        <h1>
+
+        </h1>
+        </>
+
+    );
+};
+
+export default BaseLayout;
